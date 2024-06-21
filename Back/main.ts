d626b3f622ab8495bd2f16d2da599f1507ebcccd
Davillagra <danielsebastianvillagra@hotmail.com>
2024-06-21 18:14:23 -0300

    nest-proyect

--- Back/main.ts
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("Main")
-=======
-console.log("Main-seba")
->>>>>>> cdf68daa (seba)
-=======
-console.log("Main-seba-carlos")
->>>>>>> b1a828e8 (ADD/ carlos en main)
