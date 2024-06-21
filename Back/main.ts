23cbfff51557d4a92aa1e3aba7adce571fada697
CTunjano <caratungu@gmail.com>
2024-06-21 15:44:26 -0500

    ADD/ carlos en main

--- Back/main.ts
+++ Back/main.ts
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log("Main")
 =======
 console.log("Main-seba")
 >>>>>>> cdf68daa (seba)
+=======
+console.log("Main-seba-carlos")
+>>>>>>> b1a828e8 (ADD/ carlos en main)
