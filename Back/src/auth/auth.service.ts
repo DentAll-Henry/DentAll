7db39d946f60642b0bb841f2ff8031dcb1b83303
CTunjano <caratungu@gmail.com>
2024-06-21 18:30:38 -0500

    ADD/ initial auth

---
+++ Back/src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { AuthRepository } from "./auth.repository";
+import { Auth } from "typeorm";
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly authRepository: AuthRepository) {}
+
+    async signUp(signUpInfo: Omit<Auth, 'id'>) {
+        
+    }
+}
