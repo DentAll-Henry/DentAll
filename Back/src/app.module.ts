eb0cc9a263e4884ff235606ec36f738acf581b13
Davillagra <danielsebastianvillagra@hotmail.com>
2024-06-21 20:25:31 -0300

    dentalServ

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DentalServModule } from './dentalServ/dentalServ.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeorm from './config/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    DentalServModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
