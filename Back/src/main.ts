cf10d94e335eb456dcffbf1f4d3b6519c80fa858
Davillagra <danielsebastianvillagra@hotmail.com>
2024-06-22 20:26:13 -0300

    CRUD dentalServ

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { environment } from './config/environment';
+import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.use(json());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(environment.port);
 }
 bootstrap();
