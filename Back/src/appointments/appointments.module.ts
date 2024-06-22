e246d2cc65cbc9c95a2d09a4185299ded2b737f4
jorge <coque0912@gmail.com>
2024-06-21 20:54:54 -0400

    appointmets endpoints

--- Back/src/appointments/appointments.module.ts
+++ Back/src/appointments/appointments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Appointment } from './entities/appointment.entity';
+import { AppointmentsRepository } from './appointments.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Appointment])],
   controllers: [AppointmentsController],
-  providers: [AppointmentsService],
+  providers: [AppointmentsService, AppointmentsRepository],
 })
 export class AppointmentsModule {}
--- Back/src/appointments/appointments.service.ts
+++ Back/src/appointments/appointments.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
+import { AppointmentsRepository } from './appointments.repository';
 
 @Injectable()
 export class AppointmentsService {
+  constructor(private readonly appointmentsRepository: AppointmentsRepository) {}
   create(createAppointmentDto: CreateAppointmentDto) {
     return 'This action adds a new appointment';
   }
