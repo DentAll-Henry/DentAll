b99a0ba6d58dbc7a348288105f325f4ec3055d57
jorge <coque0912@gmail.com>
2024-06-23 12:26:31 -0400

    min changes to appointments mod

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { environment } from './config/environment';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe())
   await app.listen(environment.port);
 }
 bootstrap();
