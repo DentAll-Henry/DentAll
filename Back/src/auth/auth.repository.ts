b35e5ba3a7724819282ff55df2750b1b0ce4b5db
CTunjano <caratungu@gmail.com>
2024-06-22 22:13:57 -0500

    ADD/ async function credentialByEmail

--- Back/src/auth/auth.repository.ts
+++ Back/src/auth/auth.repository.ts
@@ -9,13 +9,28 @@ export class AuthRepository {
     @InjectRepository(Auth) private authRepository: Repository<Auth>,
   ) {}
 
+  async credentialByEmail(email: string): Promise<Auth> {
+    const credential: Auth = await this.authRepository.findOne({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        email: true,
+        password: true,
+      },
+    });
+    if (!credential) throw new BadRequestException('Invalid credentials');
+    return credential;
+  }
+  
   async signUp(signUpInfo: Omit<Auth, 'id'>): Promise<string> {
     const emailExist: Auth = await this.authRepository.findOne({
-        where: {
-            email: signUpInfo.email
-        }
-    })
-    if (emailExist) throw new BadRequestException('Email already registered')
+      where: {
+        email: signUpInfo.email,
+      },
+    });
+    if (emailExist) throw new BadRequestException('Email already registered');
     const credentialCreated: Auth = await this.authRepository.save(signUpInfo);
     return credentialCreated.id;
   }
