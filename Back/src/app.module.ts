6c00f2c4d1d40c7f377f9552127718c9748f627f
CTunjano <caratungu@gmail.com>
2024-06-22 22:16:24 -0500

    FIX/ add to imports AuthModule, PeopleModule and JwtModule

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -3,6 +3,10 @@ import { DentalServModule } from './dentalServ/dentalServ.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeorm from './config/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { environment } from './config/environment';
+import { AuthModule } from './auth/auth.module';
+import { PeopleModule } from './person/person.module';
 
 @Module({
   imports: [
@@ -16,6 +20,15 @@ import typeorm from './config/typeorm';
         configService.get('typeorm'),
     }),
     DentalServModule,
+    AuthModule,
+    PeopleModule,
+    JwtModule.register({
+      global: true,
+      secret: environment.jwt,
+      signOptions: {
+        expiresIn: '1h'
+      }
+    })
   ],
   controllers: [],
   providers: [],
