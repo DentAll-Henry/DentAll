c9ef8de291bda4a7540116962e1057b344437df6
CTunjano <caratungu@gmail.com>
2024-06-22 22:11:02 -0500

    ADD/ signIn.dto

---
+++ Back/src/auth/dtos/signIn.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class SignInDto {
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'It should be a valid email',
+    example: 'example@mail.com',
+  })
+  email: string;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Enter your password',
+    example: 'Pass*123'
+  })
+  password: string;
+}
