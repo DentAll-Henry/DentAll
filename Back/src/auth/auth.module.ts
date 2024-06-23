db85906c802df99a7e852012e646d684e3c0ec77
CTunjano <caratungu@gmail.com>
2024-06-22 23:43:25 -0500

    FIX/ Delete PeopleModule from imports

--- Back/src/auth/auth.module.ts
+++ Back/src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PeopleModule } from 'src/person/person.module';
+// import { PeopleModule } from '../person/person.module';
 import { Auth } from './auth.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthRepository } from './auth.repository';
 
 @Module({
-  imports: [PeopleModule, TypeOrmModule.forFeature([Auth])],
+  imports: [TypeOrmModule.forFeature([Auth])],
   controllers: [AuthController],
   providers: [AuthService, AuthRepository],
   exports: [AuthService, TypeOrmModule],
