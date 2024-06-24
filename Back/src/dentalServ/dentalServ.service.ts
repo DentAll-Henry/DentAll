631db61c2843d7fd9d192d169e7404f5f3ad691a
Davillagra <danielsebastianvillagra@hotmail.com>
2024-06-24 11:41:30 -0300

    FIX/ service-promises

--- Back/src/dentalServ/dentalServ.service.ts
+++ Back/src/dentalServ/dentalServ.service.ts
@@ -1,20 +1,28 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { DentalServRepository } from './dentalServ.repository';
 import { DentalServDto } from './dentalServ.dto';
+import { DeleteResult } from 'typeorm';
+import { DentalServ } from './dentalServ.entity';
 
 @Injectable()
 export class DentalServService {
   constructor(private readonly dentalServRepositiory: DentalServRepository) {}
-  async getDentalServ() {
+  async getDentalServ(): Promise<DentalServ[]> {
     return await this.dentalServRepositiory.getDentalServ();
   }
-  async createDentalServ(data: DentalServDto) {
+
+  async createDentalServ(data: DentalServDto): Promise<DentalServ> {
     return await this.dentalServRepositiory.createDentalServ(data);
   }
-  async getDentalServByID(id: string) {
+
+  async getDentalServByID(id: string): Promise<DentalServ> {
     return await this.dentalServRepositiory.getDentalServByID(id);
   }
-  async editDentalServ(id: string, data: Partial<DentalServDto>) {
+
+  async editDentalServ(
+    id: string,
+    data: Partial<DentalServDto>,
+  ): Promise<DentalServ> {
     if (Object.keys(data).length === 0) {
       throw new BadRequestException(
         'You must provide name, price or description to update',
@@ -22,7 +30,7 @@ export class DentalServService {
     }
     return await this.dentalServRepositiory.editDentalServ(id, data);
   }
-  async removeDentalServ(id: string) {
+  async removeDentalServ(id: string): Promise<DeleteResult> {
     return await this.dentalServRepositiory.removeDentalServ(id);
   }
 }
