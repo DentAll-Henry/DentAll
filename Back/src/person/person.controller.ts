941f5c6911695a92d21b9822aa159cecda826082
CTunjano <caratungu@gmail.com>
2024-06-24 09:31:07 -0500

    FIX/ add post to person.controller

--- Back/src/person/person.controller.ts
+++ Back/src/person/person.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
 import { PeopleService } from './person.service';
-import { Person } from './person.entity';
+import { Person } from './entities/person.entity';
 import { ApiTags } from '@nestjs/swagger';
+import { CreatePersonDto } from './dtos/createPerson.dto';
 
 @ApiTags('People')
 @Controller('people')
 export class PeopleController {
+  constructor(private readonly peopleService: PeopleService) {}
   constructor(private readonly peopleService: PeopleService) {}
 
   @Get(':id')
@@ -14,5 +23,12 @@ export class PeopleController {
   ): Promise<Person> {
     const person: Person = await this.peopleService.personById(personId);
     return person;
+  }
+
+  @Post()
+  async createPerson(@Body() userInfo: CreatePersonDto) {
+    const { password, ...personInfo } = userInfo;
+    const authInfo = { email: userInfo.email, password: userInfo.password };
+    return this.peopleService.createPerson(personInfo, authInfo);
   }
 }
