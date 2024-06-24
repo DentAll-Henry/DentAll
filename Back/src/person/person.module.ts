5db27a03cf77895f5177340052ff59b3bd2696e6
CTunjano <caratungu@gmail.com>
2024-06-24 09:27:23 -0500

    FIX/ prettier person.module

--- Back/src/person/person.module.ts
+++ Back/src/person/person.module.ts
@@ -1,15 +1,15 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Person } from "./person.entity";
-import { PeopleController } from "./person.controller";
-import { PeopleService } from "./person.service";
-import { PeopleRepository } from "./person.repository";
-import { AuthModule } from "src/auth/auth.module";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from './entities/person.entity';
+import { PeopleController } from './person.controller';
+import { PeopleService } from './person.service';
+import { PeopleRepository } from './person.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-    imports: [AuthModule, TypeOrmModule.forFeature([Person])],
-    controllers: [PeopleController],
-    providers: [PeopleService, PeopleRepository],
-    exports: [PeopleService, TypeOrmModule]
+  imports: [AuthModule, TypeOrmModule.forFeature([Person])],
+  controllers: [PeopleController],
+  providers: [PeopleService, PeopleRepository],
+  exports: [PeopleService, TypeOrmModule],
 })
 export class PeopleModule {}
