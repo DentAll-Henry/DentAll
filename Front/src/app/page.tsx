42c440bb40b7f345b6cfbe4466bb81ae293fe74b
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-06-24 21:04:14 +0200

    fonts1

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Footer from "./components/Footers/Footer";
+import Footer from "../components/Footers/Footer";
 
 export default function Home() {
   return (
