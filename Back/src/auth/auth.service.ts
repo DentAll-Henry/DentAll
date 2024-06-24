e759cfc641c8ab8e7ff4adaa3113638f991cc8c1
CTunjano <caratungu@gmail.com>
2024-06-24 18:05:03 -0500

    FIX/ signIn and signUp

--- Back/src/auth/auth.service.ts
+++ Back/src/auth/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
     private readonly configService: ConfigService,
   ) {}
 
-  async signUp(personInfo: Partial<Person>, signUpInfo: Omit<Auth, 'id' | 'roles'>): Promise<string> {
+  async signUp(personInfo: Partial<Person>, signUpInfo: Omit<Auth, 'id'>): Promise<string> {
     const personByEmailExist: Person =
       await this.peopleService.personByEmail(personInfo.email);
     if (personByEmailExist)
@@ -35,26 +35,25 @@ export class AuthService {
     
     signUpInfo.password = await Hash(signUpInfo.password);
     
-    const newCredentialId: string = await this.authRepository.signUp(signUpInfo)
+    const newCredential: Auth = await this.authRepository.signUp(signUpInfo)
 
-    await this.peopleService.createPerson({ auth: newCredentialId, ...personInfo });
+    return await this.peopleService.createPatient({ auth: newCredential, ...personInfo });
 
-    return newCredentialId;
   }
 
-  async signIn(signInInfo: Omit<Auth, 'id' | 'roles'>) {
+  async signIn(signInInfo: Omit<Auth, 'id'>) {
     const credential: Auth = await this.authRepository.credentialByEmail(signInInfo.email);
     if (!credential) throw new BadRequestException('Invalid credentials');
     
     const isPassCorrect: boolean = await bcrypt.compare(signInInfo.password, credential.password);
     if (!isPassCorrect) throw new BadRequestException('Invalid credentials');
 
-    const person: Person = await this.peopleService.personByAuthId(credential.id)
+    const person: Person = await this.peopleService.personByEmail(credential.email)
     
     const userPayload = {
         id: person.id,
         email: credential.email,
-        roles: person.roles[0], //! Revisar cambio de rol
+        roles: person.roles[0].name, //! Revisar cambio de rol
     }
     
     const token = this.jwtService.sign(userPayload, {
