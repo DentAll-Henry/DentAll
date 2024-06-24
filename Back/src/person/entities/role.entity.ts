5c8a2b8c5187557808097c9bde2a822d4ef35231
CTunjano <caratungu@gmail.com>
2024-06-24 09:19:16 -0500

    ADD/ role.entity

---
+++ Back/src/person/entities/role.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+import { Roles } from "../roles.enum";
+
+@Entity({
+    name: 'roles',
+})
+export class Role {
+    @PrimaryGeneratedColumn('uuid')
+    id: string = uuid();
+
+    @Column()
+    name: Roles;
+
+    @Column()
+    description: string;
+}
