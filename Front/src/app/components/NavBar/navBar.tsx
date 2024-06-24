e9534142ccafca421a6e362d1b67af577a7f1563
manuochoa16 <nicomanu2316@gmail.com>
2024-06-24 14:48:36 -0300

    NavBar2

---
+++ Front/src/app/components/NavBar/navBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h4>INICIO</h4>
+        <h4>NOSOTROS</h4>
+        <h4>TRATAMIENTOS</h4>
+        <h4>SERVICIOS</h4>
+      </div>
+      <div>
+        <button>Agendar cita</button>
+        <button>Iniciar sesion</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
