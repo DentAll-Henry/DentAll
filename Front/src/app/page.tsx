ffb598c329ec5a94b0850679a05d5683ed57ec9a
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-06-25 11:13:12 +0200

    global

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import Footer from "../components/Footers/Footer";
 import Navbar from "@/components/NavBar/navBar";
+<<<<<<< Updated upstream
 import LandingPage from "@/components/LandingPage/LandingPage";
+=======
+import "./globals.css";
+>>>>>>> Stashed changes
 
 export default function Home() {
   return (
