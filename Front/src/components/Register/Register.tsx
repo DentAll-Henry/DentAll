24f4e8d52b1fbd75fae86112c416ab6dec8401ed
manuochoa16 <nicomanu2316@gmail.com>
2024-06-25 14:35:22 -0300

    boton iniciar - register full

--- Front/src/components/Register/Register.tsx
+++ Front/src/components/Register/Register.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const Register = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex w-full max-w-[1440px] h-full max-h-[1024px]">
-        <div className="w-1/2 flex flex-col items-center bg-gray-900 text-white p-8">
+    <div className="flex justify-center items-center h-screen bg-[#1D1D1D]">
+      <div className="flex w-full h-full">
+        <div className="w-1/3 flex flex-col items-center bg-[#1D1D1D] text-white p-8">
           <h2 className="text-[#ECEDF6] font-maven-pro text-[34px] font-semibold leading-normal mb-4">
             Bienvenido a Dentall
           </h2>
-          <div className="w-full max-w-[80%] flex flex-col gap-4">
+          <div className="w-[80%] flex flex-col gap-4">
             <div className="w-full">
               <label className="text-[#ECEDF6] font-mulish text-[15px] font-medium leading-normal">
                 NOMBRE Y APELLIDO
@@ -75,47 +75,53 @@ const Register = () => {
                 placeholder="Placeholder"
               />
             </div>
-            <div className="w-full max-w-[100%] mt-2 flex justify-end">
+            <div className="w-full mt-2 flex justify-end">
               <a href="#" className="text-[#00CE90] font-mulish text-[15px]">
                 Olvidé mi contraseña
               </a>
             </div>
           </div>
-          <div className="w-full max-w-[80%] mt-4 flex justify-center">
+          <div className="w-full mt-4 flex justify-center">
             <button className="flex w-[340px] h-[38px] px-[25px] py-[11px] justify-center items-center gap-[10px] rounded-[1px] bg-[#00CE90]">
               <span className="text-[#030423] font-maven-pro text-[16px] font-semibold leading-normal">
                 Crear cuenta
               </span>
             </button>
           </div>
-          <div className="w-full max-w-[80%] text-[#00CE90] mt-8 flex flex-col items-center space-y-4">
-            <a href="#">O registrate con:</a>
+          <div className="w-full text-[#00CE90] mt-8 flex flex-col items-center space-y-4">
+            <p>O registrate con:</p>
             <div className="flex space-x-4">
               <div className="w-[44px] h-[44px] flex-shrink-0 relative">
-                <Image
-                  src="/images/RS1.svg"
-                  alt="Register Image"
-                  layout="fill"
-                />
+                <a href="#">
+                  <Image
+                    src="/images/RS1.svg"
+                    alt="Register Image"
+                    layout="fill"
+                  />
+                </a>
               </div>
               <div className="w-[44px] h-[44px] flex-shrink-0 relative">
-                <Image
-                  src="/images/RS2.svg"
-                  alt="Register Image"
-                  layout="fill"
-                />
+                <a href="#">
+                  <Image
+                    src="/images/RS2.svg"
+                    alt="Register Image"
+                    layout="fill"
+                  />
+                </a>
               </div>
               <div className="w-[44px] h-[44px] flex-shrink-0 relative">
-                <Image
-                  src="/images/RS3.svg"
-                  alt="Register Image"
-                  layout="fill"
-                />
+                <a href="#">
+                  <Image
+                    src="/images/RS3.svg"
+                    alt="Register Image"
+                    layout="fill"
+                  />
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="w-1/2 h-full relative">
+        <div className="w-2/3 h-full relative">
           <Image
             src="/images/ImgReg.svg"
             alt="Register Image"
