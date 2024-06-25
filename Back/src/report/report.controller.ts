277da33f41c6db3cd4bc842e8ca0130b6ac79358
Davillagra <danielsebastianvillagra@hotmail.com>
2024-06-25 12:38:18 -0300

    FIX/ Apitags

--- Back/src/report/report.controller.ts
+++ Back/src/report/report.controller.ts
@@ -10,7 +10,9 @@ import {
 } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportDto } from './report.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Report')
 @Controller('report')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
