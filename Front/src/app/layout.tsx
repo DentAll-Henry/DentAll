9b3f96477e74fd4aa473191aa5d6c6b9f31373ec
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-06-26 23:06:24 +0200

    dashPage

--- Front/src/app/layout.tsx
+++ Front/src/app/layout.tsx
@@ -1,19 +1,24 @@
 import type { Metadata } from "next";
 import ClientRouteHandler from "@/app/ClientRouteHandle";
+import ClientRouteHandler from "@/app/ClientRouteHandle";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "DentAll",
+  title: "TechnoVID",
   description: "Sitio de venta exclusiva de Apple",
 };
 
 export default function RootLayout({
   children,
 }: {
+}: {
   children: React.ReactNode;
+}) {
 }) {
   return (
     <html lang="en">
+      <body>
+        <ClientRouteHandler>{children}</ClientRouteHandler>
       <body>
         <ClientRouteHandler>{children}</ClientRouteHandler>
       </body>
