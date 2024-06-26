037311d66a737054fa6fdf3b4937ae392ffaf09f
CTunjano <caratungu@gmail.com>
2024-06-26 06:57:18 -0500

    ADD/ getAuth0Protected function

--- Back/src/person/person.controller.ts
+++ Back/src/person/person.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Post, Req } from '@nestjs/common';
 import { PeopleService } from './person.service';
 import { Person } from './entities/person.entity';
 import { ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
 
 @ApiTags('People')
 @Controller('people')
 export class PeopleController {
   constructor(private readonly peopleService: PeopleService) {}
+
+  @Get('auth0')
+  getAuth0Protected(@Req() req: Request){
+    const jwt = require('jsonwebtoken');
+    const idToken = req.oidc.idToken;
+    console.log(idToken);
+    
+    const decodedToken = jwt.decode(idToken);
+    console.log(decodedToken['http://localhost:3000/log_count']);
+    return JSON.stringify(req.oidc.user)
+  }
 
   @Get(':id')
   async personById(
