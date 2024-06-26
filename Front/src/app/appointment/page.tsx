25e72d9fbe42375d644d9253ee8fab0b214115b5
manuochoa16 <nicomanu2316@gmail.com>
2024-06-26 16:13:04 -0300

    Appointments

---
+++ Front/src/app/appointment/page.tsx
@@ -0,0 +1,9 @@
+import Appointment from "@/components/Appointments/Appointments";
+import Login from "@/components/Appointments/Appointments";
+import React from "react";
+
+const page = () => {
+  return <Appointment />;
+};
+
+export default page;
