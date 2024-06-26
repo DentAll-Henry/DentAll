324dcd613cf8f01aefa5460b5e2f789e0e0bde12
jorge <coque0912@gmail.com>
2024-06-25 20:47:12 -0400

    swagger in appointments

--- Back/src/appointments/dto/create-appointment.dto.ts
+++ Back/src/appointments/dto/create-appointment.dto.ts
@@ -1,22 +1,48 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsUUID, IsString } from "class-validator";
 
 export class CreateAppointmentDto {
 
+    @ApiProperty({
+        example: '75277905-5676-41dd-bd26-24d688293c76',
+        description: 'Id of the dentist to be assigned. UUID format',
+        type: 'UUID',
+    })
     @IsNotEmpty()
     @IsUUID()
     dentist_id: string;
 
+    @ApiProperty({
+        example: '62a3bd93-1c50-436a-9644-cd314cf71623',
+        description: 'Id of the patient to be consulted. UUID format',
+        type: 'UUID',
+    })
     @IsNotEmpty()
     @IsUUID()
     patient: string;
 
+    @ApiProperty({
+        example: '514f20e6-ac09-4d90-99c4-26a5fa0cbaa2',
+        description: 'Id of the service to be performed. UUID format',
+        type: 'UUID',
+    })
     @IsNotEmpty()
     @IsUUID()
     service: string;
 
+    @ApiProperty({
+        example: '2022-06-06 14:00',
+        description: 'Date of the appointment',
+        format: 'YYYY-MM-DD HH:mm',
+    })
     @IsNotEmpty({ message: 'Date is required' })
     date_time: Date;
 
+    @ApiProperty({
+        description: 'Any considerations that the dentist should have. Optional, otherwise, send an empty string',
+        example: 'Many teeth are missing',
+
+    })
     @IsString()
     description: string;
     
