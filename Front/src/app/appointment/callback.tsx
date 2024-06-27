ba89b25004d35875f426494fdeff88f303fd467e
manuochoa16 <nicomanu2316@gmail.com>
2024-06-27 01:23:31 -0300

    Appointment API 3

--- Front/src/app/appointment/callback.tsx
+++ Front/src/app/appointment/callback.tsx
@@ -1,4 +1,7 @@
-// Front/src/app/appointment/callback.tsx
+// Front/src/appointment/callback.tsx
+
+"use client";
+
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { getToken } from "@/utils/authConfig";
