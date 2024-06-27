7a9b5d597891d10c4ff68398c4e2f7f0663f1d3b
Mauricio Arce <mauarce34@gmail.com>
2024-06-27 14:51:27 -0300

    /ADD swagger to Report

--- Back/src/report/report.dto.ts
+++ Back/src/report/report.dto.ts
@@ -7,28 +7,49 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ReportDto {
   @IsOptional()
+  @ApiProperty({ type: productReportDto2, isArray: true })
   products: productReportDto2[];
 
   @IsNotEmpty()
   @IsUUID()
+  @ApiProperty({
+    type: String,
+    example: '123e4567-e89b-12d3-a456-426655440000',
+  })
   appointment_id: string;
 }
 
 export class CreateReportDto {
   @IsOptional()
+  @ApiProperty({ type: productReportDto2, isArray: true })
   products: productReportDto2[];
 
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    type: Appointment,
+    example: {
+      id: '123e4567-e89b-12d3-a456-426655440000',
+      date_time: new Date(),
+      description: 'Description',
+      dentist_id: '123e4567-e89b-12d3-a456-426655440000',
+      patient: '123e4567-e89b-12d3-a456-426655440000',
+      service: '123e4567-e89b-12d3-a456-426655440000',
+    },
+  })
   @IsUUID()
   appointment: Appointment;
 }
 
 export class UpdateReportDto {
   @IsNotEmpty()
+  @ApiProperty({ type: productReportDto2, isArray: true })
   products: productReportDto2[];
 
   @IsNotEmpty()
   @IsUUID()
+  @ApiProperty({
+    type: String,
+    example: '123e4567-e89b-12d3-a456-426655440000',
+  })
   appointment_id: string;
 }
