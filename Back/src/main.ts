67d95d88b6b5601835e5549e19bfdd32ae879c98
CTunjano <caratungu@gmail.com>
2024-06-27 11:44:19 -0500

    FIX/ remove auth0 importations

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -6,15 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
 import { json } from 'express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { auth } from 'express-openid-connect';
-import { auth0Config } from './config/auth0';
 const version = require('../package.json').version;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   app.use(json());
-  app.use(auth(auth0Config));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const swaggerConfig = new DocumentBuilder()
