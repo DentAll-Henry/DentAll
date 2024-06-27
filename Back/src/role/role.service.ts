9a745b2af341a4b3c2d8a7e15a325f955158796a
Davillagra <danielsebastianvillagra@hotmail.com>
2024-06-27 11:41:05 -0300

    ADD/ repository

--- Back/src/role/role.service.ts
+++ Back/src/role/role.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { RolesRepository } from "./role.repository";
-import { Roles } from "./enums/roles.enum";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { RolesRepository } from './role.repository';
+import { Roles } from './enums/roles.enum';
 
 @Injectable()
-export class RolesService {
-    constructor(
-        private readonly rolesRespository: RolesRepository,
-    ) {}
+export class RolesService implements OnModuleInit {
+  constructor(private readonly rolesRespository: RolesRepository) {}
+  async onModuleInit() {
+    return await this.rolesRespository.init();
+  }
 
-    async getRoles() {
-        return this.rolesRespository.getRoles();
-    }
+  async getRoles() {
+    return this.rolesRespository.getRoles();
+  }
 
-    async roleByName(name: Roles) {
-        return this.rolesRespository.roleByName(name);
-    }
+  async roleByName(name: Roles) {
+    return this.rolesRespository.roleByName(name);
+  }
 
-    async createRoles() {
-        return this.rolesRespository.createRoles();
-    }
+  async createRoles() {
+    return this.rolesRespository.createRoles();
+  }
 }
