32c1d358acdd8b742a5e7a8c3de9d1d88703df12
Davillagra <danielsebastianvillagra@hotmail.com>
2024-06-27 11:41:21 -0300

    ADD/ init

--- Back/src/role/role.repository.ts
+++ Back/src/role/role.repository.ts
@@ -1,15 +1,23 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
-import { Repository } from "typeorm";
-import { Roles } from "./enums/roles.enum";
-import { rolesDB } from '../db/rolesDB'
+import { Repository } from 'typeorm';
+import { Roles } from './enums/roles.enum';
+import { rolesDB } from '../db/rolesDB';
 
 @Injectable()
 export class RolesRepository {
   constructor(
     @InjectRepository(Role) private rolesRepository: Repository<Role>,
   ) {}
+
+  async init() {
+    const rolesInDB: Role[] = await this.getRoles();
+    if (rolesInDB.length !== 0) return;
+    for (const role of rolesDB) {
+      await this.rolesRepository.save(role);
+    }
+  }
 
   async getRoles() {
     const roles: Role[] = await this.rolesRepository.find();
@@ -18,19 +26,20 @@ export class RolesRepository {
 
   async roleByName(name: Roles): Promise<Role> {
     const role: Role = await this.rolesRepository.findOne({
-        where: {
-            name,
-        }
+      where: {
+        name,
+      },
     });
-    if (!role) throw new BadRequestException('Role with that name does not exist')
+    if (!role)
+      throw new BadRequestException('Role with that name does not exist');
     return role;
   }
 
   async createRoles() {
     const rolesInDB: Role[] = await this.getRoles();
-    if (rolesInDB.length !== 0) return 'Data in Roles already exist'
-    for(const role of rolesDB) {
-      await this.rolesRepository.save(role)
+    if (rolesInDB.length !== 0) return 'Data in Roles already exist';
+    for (const role of rolesDB) {
+      await this.rolesRepository.save(role);
     }
     return 'Data in Roles loaded';
   }
