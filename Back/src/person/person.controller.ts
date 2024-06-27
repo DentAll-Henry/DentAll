6f33af8152d1fc5806aeca0a8e143d1a2d1c708e
CTunjano <caratungu@gmail.com>
2024-06-27 11:11:58 -0500

    ADD/ Request GET by email DEL/ Request GET for auth0

--- Back/src/person/person.controller.ts
+++ Back/src/person/person.controller.ts
@@ -1,30 +1,23 @@
-import { Controller, Get, Param, ParseUUIDPipe, Post, Req } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { PeopleService } from './person.service';
 import { Person } from './entities/person.entity';
 import { ApiTags } from '@nestjs/swagger';
-import { Request } from 'express';
 
 @ApiTags('People')
 @Controller('people')
 export class PeopleController {
   constructor(private readonly peopleService: PeopleService) {}
 
-  @Get('auth0')
-  getAuth0Protected(@Req() req: Request){
-    const jwt = require('jsonwebtoken');
-    const idToken = req.oidc.idToken;
-    console.log(idToken);
-    
-    const decodedToken = jwt.decode(idToken);
-    console.log(decodedToken['http://localhost:3000/log_count']);
-    return JSON.stringify(req.oidc.user)
-  }
-
   @Get(':id')
-  async personById(
-    @Param('id', ParseUUIDPipe) personId: string,
-  ): Promise<Person> {
+  async personById(@Param('id', ParseUUIDPipe) personId: string): Promise<Person> {
     const person: Person = await this.peopleService.personById(personId);
     return person;
+  }
+
+  @Get('email')
+  async personByEmail(@Param('email') email: string) {
+    const person: Person = await this.peopleService.personByEmail(email);
+    if(!person) throw new BadRequestException(`Person with email ${email} does not exist`)
+    return true;
   }
 }
