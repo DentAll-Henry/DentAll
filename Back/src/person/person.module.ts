3730a4aba820eaf2ea876b8af14f30810f934ec2
CTunjano <caratungu@gmail.com>
2024-06-27 22:59:29 -0500

    ADD/ Importation typeorm Guest

--- Back/src/person/person.module.ts
+++ Back/src/person/person.module.ts
@@ -5,9 +5,10 @@ import { PeopleController } from './person.controller';
 import { PeopleService } from './person.service';
 import { PeopleRepository } from './person.repository';
 import { RolesModule } from '../role/role.module';
+import { Guest } from './entities/guest.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Person]), RolesModule],
+  imports: [TypeOrmModule.forFeature([Person]), RolesModule, TypeOrmModule.forFeature([Guest])],
   controllers: [PeopleController],
   providers: [PeopleService, PeopleRepository],
   exports: [PeopleService, TypeOrmModule],
