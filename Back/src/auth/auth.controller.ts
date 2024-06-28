488c14034359563de7642ea9b3329354a3c8175f
CTunjano <caratungu@gmail.com>
2024-06-28 00:20:20 -0500

    FIX/ Improve swagger documentation

--- Back/src/auth/auth.controller.ts
+++ Back/src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dtos/signIn.dto';
 import { CreatePersonDto } from '../person/dtos/createPerson.dto';
@@ -10,11 +10,17 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('singin')
+  @ApiOperation({ summary: 'Validating credential for signin.' })
+  @ApiResponse({ status: 201, description: 'Return the token.', })
+  @ApiBadRequestResponse({ status: 400, description: 'Invalid credentials.' })
   signIn(@Body() signInInfo: SignInDto) {
     return this.authService.signIn(signInInfo);
   }
 
   @Post('signup')
+  @ApiOperation({ summary: 'Creating an user and its credentials.' })
+  @ApiResponse({ status: 201, description: 'Return the Auth created.', })
+  @ApiBadRequestResponse({ status: 400, description: 'Invalid credentials.' })
   signUp(@Body() userInfo: CreatePersonDto) {
     const { password, ...personInfo } = userInfo;
     const authInfo = { email: userInfo.email, password: userInfo.password };
