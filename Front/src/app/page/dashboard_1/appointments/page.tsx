68bd1c1f52dff1883f168fb35ef65c2c3894233d
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-06-28 18:29:46 +0200

    Citas

---
+++ Front/src/app/page/dashboard_1/appointments/page.tsx
@@ -0,0 +1,55 @@
+import SearchBar from '@/components/SearchBar/SearchBar';
+import Image from 'next/image';
+import React from 'react'
+import NavDash from '@/components/NavBar/navDash';
+import Link from 'next/link';
+
+const page = () => {
+  return (
+    <div className="w-[80%] h-screen bg-darkD-600 text-white ml-[20%]">
+      <NavDash />
+      <div className="flex justify-between items-center m-8 mt-24">
+        <h2 className="text-[58px] text-center  text-white font-bold leading-normal">
+          Mis <span className="text-[#00CE90]">citas</span>
+        </h2>
+      </div>
+      <div>
+        <h2 className="font-bold ml-8">PROXIMAS CITAS</h2>
+      </div>
+      <div className="flex justify-end">
+        <Link href="#">
+          <p>+ Agregar cita</p>
+        </Link>
+      </div>
+      <div className="bg-darkD-500 flex justify-around p-4 rounded-md m-8">
+        <p>Fecha</p>
+        <p>Hora</p>
+        <p>Tipo de consulta</p>
+        <p>Estado</p>
+        <Image
+          className="group-hover:fill-current text-white"
+          src="/images/NotePencil.svg"
+          width={35}
+          height={35}
+          alt="Pagos"
+        />
+        <Image
+          className="group-hover:fill-current text-white"
+          src="/images/ToggleLeft.svg"
+          width={35}
+          height={35}
+          alt="Pagos"
+        />
+      </div>
+      <div>
+        <p className="bg-darkD-500">Fecha</p>
+        <p className="bg-darkD-500">Fecha</p>
+        <p className="bg-darkD-500">Fecha</p>
+        <p className="bg-darkD-500">Fecha</p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default page
