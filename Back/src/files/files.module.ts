9febb0d5426749cfd219b746dea4b3089f36a69c
Mauricio Arce <mauarce34@gmail.com>
2024-06-28 15:03:59 -0300

    /ADD cloudinary upload

---
+++ Back/src/files/files.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { cloudinaryConfig } from 'src/config/cloudinary-config';
+import { FilesController } from './files.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [],
+  controllers: [FilesController],
+  providers: [FilesService, cloudinaryConfig],
+})
+export class FilesModule {}
