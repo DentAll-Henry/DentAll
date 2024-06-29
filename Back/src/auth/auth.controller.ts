bdbcbca23db0538933bc5dc2be7e98ee46c7df47
CTunjano <caratungu@gmail.com>
2024-06-29 00:44:00 -0500

    ADD/ restoreAuth and changePass

--- Back/src/auth/auth.controller.ts
+++ Back/src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Delete, Param, ParseUUIDPipe, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Param, ParseUUIDPipe, Patch, Post, UseInterceptors } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dtos/signIn.dto';
 import { CreatePersonDto } from '../person/dtos/createPerson.dto';
 import { SignUpInterceptor } from './interceptors/signUp.interceptor';
+import { RestoreAuthDto } from './dtos/restoreAuth.dto';
+import { UpdatePasswordDto } from './dtos/updatePassword.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -29,11 +31,27 @@ export class AuthController {
     return this.authService.signUp(personInfo, authInfo);
   }
 
-  @Delete('id')
-  @ApiOperation({ summary: 'Delete a person by ID.' })
+  @Delete('email')
+  @ApiOperation({ summary: 'Delete a person and itd credentials.' })
   @ApiResponse({ status: 201, description: 'Action confirmed.', })
   @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
-  async deleteAuth(@Param('id', ParseUUIDPipe) id: string) {
-    return this.authService.deleteAuth(id);
+  async deleteAuth(@Body() email: string) {
+    return this.authService.deleteAuth(email);
+  }
+
+  @Patch('restore')
+  @ApiOperation({ summary: 'Restore person and its credentials.' })
+  @ApiResponse({ status: 201, description: 'Return the person restored.', })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
+  async restoreAuth(@Body() authInfo: RestoreAuthDto) {
+    return this.authService.restoreAuth(authInfo);
+  }
+  
+  @Patch('changePassword')
+  @ApiOperation({ summary: 'Change the password.' })
+  @ApiResponse({ status: 201, description: 'Password updated succesfully.', })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
+  async changePass(@Body() newPass: UpdatePasswordDto) {
+    return this.authService.changePass(newPass);
   }
 }
