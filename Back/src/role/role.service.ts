fb1f1944df2414ec64f864b499a68dd4a8233f22
CTunjano <caratungu@gmail.com>
2024-06-29 00:55:53 -0500

    FIX/ getRoles and DEL/ onModuleInit and createRoles

--- Back/src/role/role.service.ts
+++ Back/src/role/role.service.ts
@@ -1,23 +1,16 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { RolesRepository } from './role.repository';
 import { Roles } from './enums/roles.enum';
 
 @Injectable()
-export class RolesService implements OnModuleInit {
+export class RolesService{
   constructor(private readonly rolesRespository: RolesRepository) {}
-  async onModuleInit() {
-    return await this.rolesRespository.init();
-  }
 
-  async getRoles() {
-    return this.rolesRespository.getRoles();
+  async getRoles(paginationDto) {
+    return this.rolesRespository.getRoles(paginationDto);
   }
 
   async roleByName(name: Roles) {
     return this.rolesRespository.roleByName(name);
-  }
-
-  async createRoles() {
-    return this.rolesRespository.createRoles();
   }
 }
