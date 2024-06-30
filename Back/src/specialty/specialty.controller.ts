84a1cf674925ebc66f99dd0d855d415e37b3262b
Mauricio Arce <mauarce34@gmail.com>
2024-06-30 18:43:02 -0300

    /ADD  CRUD controller

--- Back/src/specialty/specialty.controller.ts
+++ Back/src/specialty/specialty.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { SpecialtyService } from './specialty.service';
+import { Specialty } from './specialty.entity';
+import { specialtyDto, updateSpecialtyDto } from './specialty.dto';
 
-@Controller('specialty')
-export class SpecialtyController {}
+@Controller('speciality')
+export class SpecialtyController {
+  constructor(private readonly specialtyService: SpecialtyService) {}
+
+  @Get()
+  getSpecialities() {
+    return this.specialtyService.getSpecialties();
+  }
+
+  @Get(':id')
+  getSpecialtyById(@Param('id') id: string) {
+    return this.specialtyService.getSpecialtyById(id);
+  }
+
+  @Post()
+  createSpecialty(@Body() data: specialtyDto) {
+    return this.specialtyService.createSpecialty(data);
+  }
+
+  @Patch(':id')
+  updateSpecialty(@Param('id') id: string, @Body() data: updateSpecialtyDto) {
+    return this.specialtyService.updateSpecialty(id, data);
+  }
+
+  @Delete(':id')
+  removeSpecialty(@Param('id') id: string) {
+    return this.specialtyService.removeSpecialty(id);
+  }
+}
