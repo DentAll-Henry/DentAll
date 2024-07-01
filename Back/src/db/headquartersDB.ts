836ea54f3ed03de26ae217899f4f1c0c722c86f9
jorge <coque0912@gmail.com>
2024-07-01 11:29:38 -0400

    fixes in mock

--- Back/src/db/headquartersDB.ts
+++ Back/src/db/headquartersDB.ts
@@ -1,20 +1,21 @@
 export const headquartersDB = [
   {
-    name: 'Headquarters 1',
-    address: '123 Main Street, City, Country',
-    img: 'http://example.com/image1.jpg',
+
+    name: "Headquarters 1",
+    address: "123 Main Street, City, Country",
+    img: "http://example.com/image1.jpg",
     cords: {
       lat: 37.7749295,
-      lng: -122.4194155,
-    },
+      lng: -122.4194155
+    }
   },
   {
-    name: 'Headquarters 2',
-    address: '456 Another Ave, Another City, Another Country',
-    img: 'http://example.com/image2.jpg',
+    name: "Headquarters 2",
+    address: "456 Another Ave, Another City, Another Country",
+    img: "http://example.com/image2.jpg",
     cords: {
       lat: 34.0522342,
-      lng: -118.2436849,
-    },
-  },
+      lng: -118.2436849
+    }
+  }
 ];
