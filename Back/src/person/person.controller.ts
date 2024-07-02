c65f5ab35704a9b5fbbb40a0c3bae286371b61f5
CTunjano <caratungu@gmail.com>
2024-07-02 11:52:53 -0500

    ADD/ new functions and FIX/ dtos

--- Back/src/person/person.controller.ts
+++ Back/src/person/person.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   ParseUUIDPipe,
   Patch,
+  Post,
   Query,
 } from '@nestjs/common';
 import { PeopleService } from './person.service';
@@ -21,6 +22,10 @@ import { LimitApiQueries, PageApiQueries } from '../config/swagger-config';
 import { PaginationDto } from '../common/dto/paginationDto';
 import { Guest } from './entities/guest.entity';
 import { ChangeRoleDto } from './dtos/changeRoles.dto';
+import { AuthByEmailDto } from 'src/auth/dtos/authByEmail.dto';
+import { UpdatePersonDto } from './dtos/updatePerson.dto';
+import { CreatePersonDto } from './dtos/createPerson.dto';
+import { EditPersonDto } from './dtos/editPerson.dto';
 
 @ApiTags('People')
 @Controller('people')
@@ -36,27 +41,17 @@ export class PeopleController {
   async getAllPeople(@Query() paginationDto: PaginationDto) {
     return this.peopleService.getAllPeople(paginationDto);
   }
-  
-  @Get('email')
-  @ApiOperation({ summary: 'Get a person by email.' })
-  @ApiResponse({
-    status: 200,
-    description: 'Returns to the person with the specified email.',
-  })
-  @ApiBadRequestResponse({
-    status: 400,
-    description: 'Person with that email does not exist.',
-  })
-  async personByEmail(@Param('email') email: string) {
-    const person: Person = await this.peopleService.personByEmail(email);
-    if (!person)
-      throw new BadRequestException(
-        `Person with email ${email} does not exist`,
-      );
-    return true;
+
+  @Get('wd')
+  @ApiOperation({ summary: 'Get all people.' })
+  @ApiResponse({ status: 200, description: 'Return an array with all people.' })
+  @ApiQuery(PageApiQueries)
+  @ApiQuery(LimitApiQueries)
+  async getAllPeopleIncludeDeleted(@Query() paginationDto: PaginationDto) {
+    return this.peopleService.getAllPeopleIncludeDeleted(paginationDto);
   }
 
-  @Get(':idPerson')
+  @Get(':idperson')
   @ApiOperation({ summary: 'Get a person by ID.' })
   @ApiResponse({
     status: 200,
@@ -67,9 +62,53 @@ export class PeopleController {
     description: 'Person with that ID does not exist.',
   })
   async personById(
-    @Param('idPerson', ParseUUIDPipe) personId: string,
+    @Param('idperson', ParseUUIDPipe) idperson: string,
   ): Promise<Person> {
-    const person: Person = await this.peopleService.personById(personId);
+    const person: Person = await this.peopleService.personById(idperson);
+    if (!person)
+      throw new BadRequestException(
+        `No existe usuario con el ID ${idperson}.`,
+      );
+    return person;
+  }
+  
+  @Get('wd/:idperson')
+  @ApiOperation({ summary: 'Get a person by ID.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns to the person with the specified ID.',
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Person with that ID does not exist.',
+  })
+  async personByIdIncludeDeleted(
+    @Param('idperson', ParseUUIDPipe) idperson: string,
+  ): Promise<Person> {
+    const person: Person = await this.peopleService.personByIdIncludeDeleted(idperson);
+    if (!person)
+      throw new BadRequestException(
+        `No existe usuario con el ID ${idperson}.`,
+      );
+    return person;
+  }
+  
+  @Get('byemail')
+  @ApiOperation({ summary: 'Get a person by email.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns to the person with the specified email.',
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Person with that email does not exist.',
+  })
+  async personByEmail(@Body() email: AuthByEmailDto) {
+    const person: Person = await this.peopleService.personByEmail(email.email);
+    if (!person)
+      throw new BadRequestException(
+        `No existe usuario con el email ${email.email}.`,
+      );
     return person;
   }
 
@@ -93,6 +132,17 @@ export class PeopleController {
   @ApiBadRequestResponse({ status: 400, description: 'Role does not exist.' })
   async delRole(@Param('idperson', ParseUUIDPipe) idperson: string, @Body() roleName: ChangeRoleDto) {
     return await this.peopleService.delRole(idperson, roleName);
+  }
+
+  @Patch('update/:id')
+  @ApiOperation({ summary: 'Update person info.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns to the updated person.',
+  })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
+  async updatePerson(@Param('id', ParseUUIDPipe) id: string, @Body() personInfo: EditPersonDto) {
+    return this.peopleService.updatePerson(id, personInfo)
   }
 
   //& --> guests endpoints <--
