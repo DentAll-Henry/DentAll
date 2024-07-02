a60226af685d64221204d86c5f34438c2946b163
manuochoa16 <nicomanu2316@gmail.com>
2024-07-02 20:31:33 -0300

    componente comentado/sin uso

--- Front/src/app/appointment/page.tsx
+++ Front/src/app/appointment/page.tsx
@@ -1,17 +1,17 @@
-// Front/src/app/appointment/page.tsx
-import React from "react";
-import Appointment from "@/components/Appointments/Appointments";
-import { getAuthUrl } from "@/utils/authConfig";
+// // Front/src/app/appointment/page.tsx
+// import React from "react";
+// import Appointment from "@/components/Appointments/Appointments";
+// import { getAuthUrl } from "@/utils/authConfig";
 
-const Page = () => {
-  const authUrl = getAuthUrl();
+// const Page = () => {
+//   const authUrl = getAuthUrl();
 
-  return (
-    <div>
-      <a href={authUrl}>Login with Google</a>
-      <Appointment />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <a href={authUrl}>Login with Google</a>
+//       <Appointment />
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
