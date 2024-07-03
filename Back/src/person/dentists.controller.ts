7b38beeed73a3529aaa356f34d79d29bc2302a27
CTunjano <caratungu@gmail.com>
2024-07-02 19:54:24 -0500

    ADD/ endpoints

--- Back/src/person/dentists.controller.ts
+++ Back/src/person/dentists.controller.ts
@@ -2,12 +2,15 @@ import { Body, Controller, Get, Param, ParseUUIDPipe, Patch, Post, Query } from 
 import {
   ApiBadRequestResponse,
   ApiOperation,
+  ApiQuery,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { DentistsService } from './dentist.service';
 import { CreateDentistDto } from './dtos/createDentist.dto';
 import { PaginationDto } from 'src/common/dto/paginationDto';
+import { LimitApiQueries, PageApiQueries } from 'src/config/swagger-config';
+import { SpecialtyNameDto } from './dtos/dentSpecialty.dto';
 
 @ApiTags('Dentists')
 @Controller('dentists')
@@ -20,9 +23,32 @@ export class DentistsController {
     status: 200,
     description: 'Returns the information of all dentists.',
   })
-  @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
+  @ApiQuery(PageApiQueries)
+  @ApiQuery(LimitApiQueries)
   async getAllDentists(@Query() paginationDto: PaginationDto) {
     return this.dentistsService.getAllDentists(paginationDto)
+  }
+
+  @Get('best4')
+  @ApiOperation({ summary: 'Get the four best rated dentists.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the 4 best rated dentists.',
+  })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
+  async get4BestRateDentists() {
+    return this.dentistsService.get4BestRateDentists();
+  }
+
+  @Get('byspecialty')
+  @ApiOperation({ summary: 'Get dentists by specialty.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns all dentists with the specified specialty.',
+  })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
+  async dentistsBySpecialty(@Query() specialtyDto: SpecialtyNameDto) {
+    return this.dentistsService.dentistsBySpecialty(specialtyDto.specialtyName)
   }
 
   @Get(':id')
@@ -34,6 +60,17 @@ export class DentistsController {
   @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
   async dentistById(@Param('id', ParseUUIDPipe) id: string) {
     return this.dentistsService.dentistById(id);
+  }
+
+  @Get('person/:id')
+  @ApiOperation({ summary: 'Get a dentist by person ID.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns to the dentist with the specified person ID.',
+  })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request.' })
+  async dentistByPersonId(@Param('id', ParseUUIDPipe) id: string) {
+    return this.dentistsService.dentistByPersonId(id);
   }
 
   @Post('create')
