e4312bd9c91e9db0886e6f47c43108315eb839c5
CTunjano <caratungu@gmail.com>
2024-07-03 14:33:14 -0500

    ADD/ functions with DentalServ

--- Back/src/person/dentist.service.ts
+++ Back/src/person/dentist.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { DentistsRepository } from './dentist.repository';
 import { Person } from './entities/person.entity';
 import { Roles } from 'src/role/enums/roles.enum';
@@ -6,6 +6,8 @@ import { Dentist } from './entities/dentist.entity';
 import { SpecialtyService } from 'src/specialty/specialty.service';
 import { PeopleService } from './person.service';
 import { Specialty } from 'src/specialty/specialty.entity';
+import { DentalServ } from 'src/dentalServ/entities/dentalServ.entity';
+import { DentalServService } from 'src/dentalServ/dentalServ.service';
 
 @Injectable()
 export class DentistsService {
@@ -13,6 +15,7 @@ export class DentistsService {
     private readonly dentistsRepository: DentistsRepository,
     private readonly peopleService: PeopleService,
     private readonly specialtiesService: SpecialtyService,
+    private readonly dentalServService: DentalServService,
   ) {}
 
   async getAllDentists(paginationDto: { page: number; limit: number }) {
@@ -27,12 +30,16 @@ export class DentistsService {
     return this.dentistsRepository.dentistsBySpecialty(specialtyName);
   }
 
-  async dentistById(id: string) {
-    return this.dentistsRepository.dentistById(id);
+  async dentistsByDentalServ(dentalServId: DentalServ['id']) {
+    return this.dentistsRepository.dentistsByDentalServ(dentalServId);
   }
 
   async dentistByPersonId(idperson: Person['id']) {
     return this.dentistsRepository.dentistByPersonId(idperson);
+  }
+  
+  async dentistById(id: string) {
+    return this.dentistsRepository.dentistById(id);
   }
 
   async createDentist(dentistInfo: {
@@ -73,5 +80,14 @@ export class DentistsService {
   async changeDentistStatus(id: string) {
     const dentist: Dentist = await this.dentistById(id);
     return this.dentistsRepository.changeDentistStatus(dentist);
+  }
+
+  async addDentalServ(id: string, dentalServId: DentalServ['id']) {
+    const dentalServ: DentalServ = await this.dentalServService.getDentalServByID(dentalServId);
+    if (!dentalServ) throw new BadRequestException('No existe un servicio dental con ese ID.');
+    const dentist: Dentist = await this.dentistById(id);
+    if (!dentist) throw new BadRequestException('No existe dentista con ese ID.');
+    const dentistWithDentalServ: Dentist = await this.dentistsRepository.addDentalServ(dentist, dentalServ);
+    return dentistWithDentalServ;
   }
 }
