099fb25affef49cb3190a847f1eb238bb0168e6a
Mauricio Arce <mauarce34@gmail.com>
2024-07-05 13:16:16 -0300

    FIX/ path for cloudinary upload (service)

--- Back/src/files/files.service.ts
+++ Back/src/files/files.service.ts
@@ -1,26 +1,38 @@
 import { Body, Injectable } from '@nestjs/common';
 import { UploadApiResponse, v2 } from 'cloudinary';
 import * as toStream from 'buffer-to-stream';
+import { fileUploadDto } from './files.dto';
+import { log } from 'console';
 
 @Injectable()
 export class FilesService {
   constructor() {}
 
-  uploadFile(
-    file: Express.Multer.File,
-    path: string,
-  ): Promise<UploadApiResponse> {
+  uploadFile(fileInfo: fileUploadDto): Promise<UploadApiResponse> {
+    const { path, file } = fileInfo;
+
+    console.log(typeof path);
+
+    console.log('Uploading file to Cloudinary');
+    console.log('File buffer length:', file.buffer.length);
+    console.log('Upload path:', path);
     return new Promise((resolve, reject) => {
+      log('Uploading file to Cloudinary2');
       const upload = v2.uploader.upload_stream(
         { resource_type: 'auto', folder: path },
+
         (error, result) => {
+          console.log('Uploading file to Cloudinary3');
           if (error) {
+            console.error('Cloudinary upload error:', error);
             reject(error);
           } else {
+            console.log('Cloudinary upload result:', result);
             resolve(result);
           }
         },
       );
+      log('Uploading file to Cloudinary4');
       toStream(file.buffer).pipe(upload);
     });
   }
