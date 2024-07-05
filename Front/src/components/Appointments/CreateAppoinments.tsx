dfd7aae1058756264177879b8a1d522ccea64315
caratungu <caratungu@gmail.com>
2024-07-05 14:50:20 -0500

    FIX/ create appointment

--- Front/src/components/Appointments/CreateAppoinments.tsx
+++ Front/src/components/Appointments/CreateAppoinments.tsx
@@ -27,11 +27,8 @@ type Dentist = {
 };
 
 const CreateAppointment = () => {
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
   const [consultationType, setConsultationType] = useState("");
   const [dentist, setDentist] = useState("");
-  const [status, setStatus] = useState("Activo");
   const [errorMessage, setErrorMessage] = useState("");
   const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
@@ -39,7 +36,18 @@ const CreateAppointment = () => {
   const [dentists, setDentists] = useState<Dentist[]>([]);
   const [slots, setSlots] = useState<any[]>([]);
   const [hours, setHours] = useState<string[]>([]);
+  const [days, setDays] = useState<string[]>([]);
   const [selectedDay, setSelectedDay] = useState("");
+  const [selectedHour, setSelectedHour] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [appointment, setAppointment] = useState({
+    dentist_id: "",
+    patient: "",
+    service: "",
+    date_time: "",
+    description: "",
+  });
 
   const getData = async () => {
     if (user?.id) {
@@ -47,6 +55,7 @@ const CreateAppointment = () => {
         const patient = await axios.get(
           `${enviroment.apiUrl}/patients/person/${user.id}`
         );
+        setAppointment({ ...appointment, patient: patient.data.id });
         const response = await axios.get(
           `${enviroment.apiUrl}/appointments/pending_appointments_by_patient/${patient.data.id}`
         );
@@ -69,42 +78,87 @@ const CreateAppointment = () => {
       const dentistResponse = await axios.get(
         `${enviroment.apiUrl}/dentists/bydentalserv?name=${encodedServiceName}`
       );
+      const serviceResponse = await axios.get(
+        `${enviroment.apiUrl}/dental-serv/by-name?name=${encodedServiceName}`
+      );
+      setAppointment({ ...appointment, service: serviceResponse.data[0].id });
       setDentists(dentistResponse.data);
     } catch (error) {
       console.error("Error fetching dentist data:", error);
     }
   };
 
+  const handleYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedYear = e.target.value;  // Mantener como string
+    setYear(selectedYear);
+  }
+
+  const handleMonthChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedMonth = e.target.value;  // Mantener como string
+    setMonth(selectedMonth)
+  }
+
   const handleDentistChange = async (
     e: React.ChangeEvent<HTMLSelectElement>
   ) => {
     const selectedDentistId = e.target.value;
     setDentist(e.target.value);
+    setAppointment({ ...appointment, dentist_id: selectedDentistId });
     try {
-      const slotsResponse = await axios.post(
+      const slotsDaysResponse = await axios.post(
         `${enviroment.apiUrl}/appointments/get_available_slots`,
         {
           dentist_id: selectedDentistId,
-          start_date: "2024-07-01",
-          end_date: "2024-07-31",
+          start_date: `${year}-${month}-${(new Date(new Date().getFullYear(), parseInt(month) + 1, 1).getDate()).toString().padStart(2, '0')}`,
+          end_date: `${year}-${month}-${(new Date(new Date().getFullYear(), parseInt(month) + 2, 0).getDate()).toString().padStart(2, '0')}`,
+          time_slots: false,
         }
       );
-      setSlots(slotsResponse.data.availabity);
+      setSlots(slotsDaysResponse.data.availabity[0]);
+      const slotsByDay = slotsDaysResponse.data.availabity[0].map((d: string) =>{
+        return (
+        new Date(d[0]).toLocaleDateString([], {
+          year: "numeric",
+          month: "short",
+          day: "2-digit",
+          timeZone: "UTC",
+        }))}
+      );
+      setDays(slotsByDay);
     } catch (error) {
       console.error("Error fetching dentist data:", error);
     }
   };
 
-  const handleDaysChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedDay = e.target.value; // Mantener como string
-    setSelectedDay(selectedDay);
-    const slotsHours = slots[parseInt(selectedDay, 10)].map((slot: string) =>
-      new Date(slot).toLocaleTimeString([], {
+    const handleDaysChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedDayNumber = e.target.value;  // Mantener como string
+    const selectedDayText = days[parseInt(selectedDayNumber)];
+    console.log(selectedDayText)
+    const slotsHoursResponse = await axios.post(
+      `${enviroment.apiUrl}/appointments/get_available_slots`,
+      {
+        dentist_id: dentist,
+        start_date: slots[parseInt(selectedDayNumber)][0].split('T')[0],
+        end_date: slots[parseInt(selectedDayNumber)][0].split('T')[0],
+        time_slots: true,
+      }
+    );
+    setSelectedDay(selectedDayNumber);
+    const slotsHours = slotsHoursResponse.data.availabity[0].map((slot: string) => {
+      return new Date(slot).toLocaleTimeString([], {
+        hour12: false,
         hour: "2-digit",
         minute: "2-digit",
-      })
+        timeZone: "UTC",
+      })}
     );
     setHours(slotsHours);
+  };
+
+  const handleHoursChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedHour = e.target.value;
+    setSelectedHour(selectedHour);
+    setAppointment({ ...appointment, date_time: `${year}-${month}-${parseInt(selectedDay)+1} ${selectedHour}` });
   };
 
   useEffect(() => {
@@ -134,24 +188,12 @@ const CreateAppointment = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!date || !time || !consultationType || !dentist) {
-      setErrorMessage("Por favor, complete todos los campos");
-      return;
-    }
-
     try {
-      const response = await axios.post(`${enviroment.apiUrl}/appointments`, {
-        date,
-        time,
-        consultationType,
-        status,
-        dentist,
-        userId: user?.id,
-      });
+      const response = await axios.post(`${enviroment.apiUrl}/appointments`, appointment );
 
       if (response.status === 201) {
         alert("Cita creada con éxito");
-        router.push("/appointments"); // Cambia "/appointments" a la ruta adecuada
+        router.push("/page/patients/appointments"); // Cambia "/appointments" a la ruta adecuada
       }
     } catch (error) {
       console.error("Error creating appointment:", error);
@@ -190,6 +232,44 @@ const CreateAppointment = () => {
               </select>
             </div>
             <div className="mb-4">
+              <label className="text-white">Año</label>
+              <select
+                value={year}
+                onChange={handleYearChange}
+                className="w-full p-2 rounded-md text-black"
+              >
+                <option value="" disabled hidden>
+                  Selecciona el año
+                </option>
+                <option value="2024">2024</option>
+                <option value="2025">2025</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label className="text-white">Mes</label>
+              <select
+                value={month}
+                onChange={handleMonthChange}
+                className="w-full p-2 rounded-md text-black"
+              >
+                <option value="" disabled hidden>
+                  Selecciona el mes
+                </option>
+                <option value="01">Enero</option>
+                <option value="02">Febrero</option>
+                <option value="03">Marzo</option>
+                <option value="04">Abril</option>
+                <option value="05">Mayo</option>
+                <option value="06">Junio</option>
+                <option value="07">Julio</option>
+                <option value="08">Agosto</option>
+                <option value="09">Septiembre</option>
+                <option value="10">Octubre</option>
+                <option value="11">Noviembre</option>
+                <option value="12">Dicembre</option>
+              </select>
+            </div>
+            <div className="mb-4">
               <label className="text-white">Dentista</label>
               <select
                 value={dentist}
@@ -216,18 +296,19 @@ const CreateAppointment = () => {
                 <option value="" disabled hidden>
                   Selecciona el día
                 </option>
-                {slots.map((d, index) => (
-                  <option key={index} value={index.toString()}>
-                    {index + 1}
-                  </option>
-                ))}
+                {days.map(
+                  (d, index) =>
+                      <option key={index} value={index.toString()}>
+                        {d}
+                      </option>
+                )}
               </select>
             </div>
             <div className="mb-4">
               <label className="text-white">Hora</label>
               <select
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
+                value={selectedHour}
+                onChange={handleHoursChange}
                 className="w-full p-2 rounded-md text-black"
               >
                 <option value="" disabled hidden>
