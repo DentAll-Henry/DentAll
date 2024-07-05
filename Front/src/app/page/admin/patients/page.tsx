8c856fe8a6f8b632f59d43e4cdfe3395abc90215
rafagarfield <mgrafaelgarcia.2805@gmail.com>
2024-07-05 13:39:06 -0500

    cambio en admin patient

--- Front/src/app/page/admin/patients/page.tsx
+++ Front/src/app/page/admin/patients/page.tsx
@@ -1,8 +1,9 @@
 import NavDash from '@/components/NavBar/navDash'
-import React from 'react'
+import React, { useState } from 'react'
 import TotalPatiens from '@/components/TotalPatients/TotalPatiens'
 
 function page() {
+ 
   return (
     <div className="w-[80%] h-screen  text-white ml-[20%] ">
         <NavDash />
