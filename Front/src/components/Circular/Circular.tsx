b9766f726bed5b344b9f0c2620f0f3c5f3cc3ea6
rafagarfield <mgrafaelgarcia.2805@gmail.com>
2024-07-08 09:16:29 -0500

    creacion del componente circular gradiente

---
+++ Front/src/components/Circular/Circular.tsx
@@ -0,0 +1,9 @@
+// components/Circular.tsx
+import React from 'react';
+import styles from './Circular.module.css';
+
+const Circular: React.FC = () => {
+  return <div className={styles.circular}></div>;
+};
+
+export default Circular;
