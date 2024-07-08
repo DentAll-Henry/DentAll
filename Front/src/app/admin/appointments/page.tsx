7bbde267a9fb998598b3a70eea7e9453c9f645a4
jorge <coque0912@gmail.com>
2024-07-08 01:00:02 -0400

    MODIFY modified for testing

--- Front/src/app/admin/appointments/page.tsx
+++ Front/src/app/admin/appointments/page.tsx
@@ -1,17 +1,19 @@
 import NavDash from '@/components/NavBar/navDash'
 import React, { useState } from 'react'
 import TotalAppointments from '@/components/TotalAppointments/TotalAppointments'
+import CalendarAppointments from '@/components/Calendar/CalendarAppointments'
 function page() {
- 
+
   return (
     <div className="w-[80%] h-screen  text-white ml-[20%] ">
-        <NavDash />
-        
-        <div className="m-8 p-4 mt-24">
-            <TotalAppointments/>
+      <NavDash />
+
+      <div className="m-8 p-4 mt-24">
+        <CalendarAppointments />
+          {/* <TotalAppointments/> */}
         </div>
 
-      
+
     </div>
   )
 }
