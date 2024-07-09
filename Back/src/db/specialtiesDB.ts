43c9a6aaa8708358500d84b0e80ffe8f30c8350b
caratungu <caratungu@gmail.com>
2024-07-09 16:15:14 -0500

    FIX/ specialty name

---
+++ Back/src/db/specialtiesDB.ts
@@ -0,0 +1,59 @@
+import { specialtyDto } from "src/specialty/specialty.dto";
+
+export const specialtiesDB = [
+    {
+        name: "Odontología general",
+        description: "La odontología general se centra en el cuidado dental integral, abarcando desde exámenes regulares y limpiezas hasta tratamientos restaurativos como empastes y coronas.",
+        services: []
+    },
+    {
+        name: "Odontología estética",
+        description: "La odontología estética se enfoca en mejorar la apariencia de la sonrisa y la estética dental mediante tratamientos como blanqueamiento dental, carillas de porcelana y ortodoncia invisible.",
+        services: []
+    },
+    {
+        name: "Odontopediatría",
+        description: "La odontopediatría se dedica al cuidado dental de los niños y adolescentes, abarcando desde la prevención de caries hasta el tratamiento de maloclusiones y traumatismos dentales.",
+        services: []
+    },
+    {
+        name: "Odontología deportiva",
+        description: "La odontología deportiva se especializa en el cuidado dental de los atletas, previniendo y tratando lesiones bucodentales relacionadas con la práctica deportiva.",
+        services: []
+    },
+    {
+        name: "Odontología preventiva",
+        description: "La odontología preventiva se centra en la prevención de enfermedades bucodentales y la promoción de la salud oral a largo plazo mediante exámenes regulares y tratamientos preventivos.",
+        services: []
+    },
+    {
+        name: "Odontología ocupacional",
+        description: "La odontología ocupacional se especializa en la evaluación y tratamiento de problemas dentales relacionados con el entorno laboral, centrándose en la prevención de lesiones bucodentales.",
+        services: []
+    },
+    {
+        name: "Ortodoncista",
+        description: "La ortodoncia se enfoca en el diagnóstico, prevención y tratamiento de problemas de maloclusión y alineación dental utilizando dispositivos como brackets y alineadores transparentes.",
+        services: []
+    },
+    {
+        name: "Periodontista",
+        description: "La periodoncia se enfoca en el diagnóstico, prevención y tratamiento de enfermedades de las encías y estructuras de soporte dental, ofreciendo tratamientos como la limpieza profunda y la colocación de implantes dentales.",
+        services: []
+    },
+    {
+        name: "Endodoncia",
+        description: "La endodoncia trata problemas dentro de los dientes, especialmente cuando la pulpa está dañada, mediante tratamientos de conducto para eliminar la pulpa enferma y sellar el diente.",
+        services: []
+    },
+    {
+        name: "Cirujano oral",
+        description: "La cirugía oral se centra en el diagnóstico y tratamiento quirúrgico de condiciones y lesiones en la boca, la mandíbula y la cara, realizando procedimientos como extracciones complicadas e implantes dentales.",
+        services: []
+    },
+    {
+        name: "Prostodoncia",
+        description: "La prostodoncia se enfoca en la restauración y reemplazo de dientes perdidos o dañados mediante la fabricación de prótesis dentales como coronas, puentes y dentaduras, así como la colocación de implantes dentales.",
+        services: []
+    }
+]
