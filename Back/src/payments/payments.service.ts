9eb40f22c830fc843be317f02c67333a7e2cff7c
jorge <coque0912@gmail.com>
2024-07-10 14:53:35 -0400

    ADD function to return all payments and details for one payment

--- Back/src/payments/payments.service.ts
+++ Back/src/payments/payments.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PaymentsRepository } from './payments.repository';
 import { PaymentDto } from './dto/payment.dto';
+import { PatientsService } from 'src/person/patient.service';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly paymentsRepository: PaymentsRepository) {}
+  constructor(
+    private readonly paymentsRepository: PaymentsRepository,
+    private readonly patientsService: PatientsService
+  ) { }
 
   async createPreference(data: PaymentDto, baseUrl: string) {
     return this.paymentsRepository.createPreference(data, baseUrl);
@@ -12,6 +16,16 @@ export class PaymentsService {
 
   async success(data) {
     return await this.paymentsRepository.success(data);
+  }
+
+  async getPaymentsByPatient(patient_id: string) {
+    const patient = await this.patientsService.patientById(patient_id);
+    if (!patient) throw new Error('Paciente no encontrado con el id proporcionado');
+    return await this.paymentsRepository.getPaymentsByPatient(patient_id);
+  }
+
+  async getPaymentById(payment_id: string) {
+    return await this.paymentsRepository.getPaymentById(payment_id);
   }
 
   // async failure(data) {
