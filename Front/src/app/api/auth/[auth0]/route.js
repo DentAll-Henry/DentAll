b55679d8aa9d40fabeda7b697e2f1d7600af7913
caratungu <caratungu@gmail.com>
2024-07-11 16:01:03 -0500

    FIX/ some tests

--- Front/src/app/api/auth/[auth0]/route.js
+++ Front/src/app/api/auth/[auth0]/route.js
@@ -1,3 +1,31 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+// import { handleAuth } from '@auth0/nextjs-auth0';
+// import { handleCallback } from '@auth0/nextjs-auth0';
+// import { callback as CB} from '../callback'
 
-export const GET = handleAuth();
+// export const GET = handleAuth({
+//     callback: async (req, res) => {
+//         try {
+//            return await handleCallback(req, res, {
+//                 CB()
+//             });
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     }
+// })
+
+import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
+import { redirect } from 'next/navigation';
+
+const afterCallback = (req, session, state) => {
+  if (!session.user) {
+    return session;
+  } else {
+    console.log(session)
+    redirect('/register-auth0');
+  }
+};
+
+export const GET = handleAuth({
+  callback: handleCallback({ afterCallback })
+});
