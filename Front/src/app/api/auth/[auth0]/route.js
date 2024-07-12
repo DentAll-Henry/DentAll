ea3af10d0da7bf8ace4b3d6ec7a23f5f9b91e5d2
caratungu <caratungu@gmail.com>
2024-07-12 11:27:04 -0500

    FIX/ environment variable

--- Front/src/app/api/auth/[auth0]/route.js
+++ Front/src/app/api/auth/[auth0]/route.js
@@ -3,6 +3,6 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
   login: handleLogin({
-    returnTo: `${enviroment.apiUrl}/register-auth0`,
+    returnTo: `${enviroment.auth0.url_base}/register-auth0`,
   })
 });
