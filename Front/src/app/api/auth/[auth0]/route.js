7448424c236f80c5c55f059319afcc161b3dce38
caratungu <caratungu@gmail.com>
2024-07-12 11:10:35 -0500

    FIX/ call .env.local

--- Front/src/app/api/auth/[auth0]/route.js
+++ Front/src/app/api/auth/[auth0]/route.js
@@ -1,37 +1,8 @@
-// import { handleAuth } from '@auth0/nextjs-auth0';
-// import { handleCallback } from '@auth0/nextjs-auth0';
-// import { callback as CB} from '../callback'
-
-// export const GET = handleAuth({
-//     callback: async (req, res) => {
-//         try {
-//            return await handleCallback(req, res, {
-//                 CB()
-//             });
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
-// })
-
+import { enviroment } from '@/utils/config';
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-/* 
-const afterCallback = (req, session, state) => {
-  console.log("req", req)
-  console.log("session", session)
-  console.log("state", state)
-
-  if (session.user) {
-    headers.set('location', '/admin');
-    return session;
-  } else {
-    console.log(session)
-    redirect('/register-auth0');
-  }
-}; */
 
 export const GET = handleAuth({
   login: handleLogin({
-    returnTo: 'http://localhost:3001/register-auth0',
+    returnTo: `${enviroment.apiUrl}/register-auth0`,
   })
 });
