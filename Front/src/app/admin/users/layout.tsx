c7c719ac917583a06d745981d2712aa1208e6688
rafagarfield <mgrafaelgarcia.2805@gmail.com>
2024-07-14 11:52:13 -0500

    nuevo layout para users

---
+++ Front/src/app/admin/users/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    
+    <>
+    {children}
+    <div className="flex-grow mt-24">
+        <div className="flex flex-col gap-4 text-orange-300 justify-end p-4 ">
+            
+            <Link className ="bg-[#222222] border rounded-md px-3 py-1 text-white hover:bg-[#454545]" href="/admin/users/patients">pacientes</Link>
+            <Link className="bg-[#222222] border rounded-md px-3 py-1 text-white hover:bg-[#454545]" href="/admin/users/dentists">dentistas</Link>
+            
+        </div>
+    </div>
+    
+    </>
+  );
+}
