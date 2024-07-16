a88920e3d876afc0efbbe93ce9db47d329ccb050
caratungu <caratungu@gmail.com>
2024-07-16 16:29:08 -0500

    FIX/ YouTubeVideo importation

--- Front/src/app/patients/recommendations/page.tsx
+++ Front/src/app/patients/recommendations/page.tsx
@@ -1,6 +1,6 @@
 import NavDash from "@/components/NavBar/navDash";
 import VideoPlayer from "@/components/Video/Video";
-import YouTubeVideo from "@/components/Video/youTubeVideo";
+import YouTubeVideo from "@/components/Video/YouTubeVideo";
 
 const page: React.FC = () => {
   return (
