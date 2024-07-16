5a3ee1f4407aad0f781a8af723286baeb720550f
Mauricio Arce <mauarce34@gmail.com>
2024-07-16 13:59:24 -0300

    FIX/ axios to axiosInstance

--- Front/src/helpers/dentist.helper.ts
+++ Front/src/helpers/dentist.helper.ts
@@ -1,28 +1,28 @@
 import axios from "axios";
 import { enviroment } from "@/utils/config";
+import axiosInstance from "@/utils/axiosInstance";
 
 export async function allDentist() {
   try {
-    const response = await axios.get(`${enviroment.apiUrl}/dentists/`);
-    console.log("este es el response",response)
-    
+    const response = await axiosInstance.get(`/dentists/`);
+    console.log("este es el response", response);
+
     return response.data;
   } catch (error) {
-    console.error("aca hay error",error);
+    console.error("aca hay error", error);
     throw error;
   }
 }
 
-
-
-export async function getDentistId(id: string){
+export async function getDentistId(id: string) {
   try {
     console.log("Soy el ID ", id);
-    const response = await axios.get(`${enviroment.apiUrl}/dentists/${id}`);
+    const response = await axiosInstance.get(
+      `${enviroment.apiUrl}/dentists/${id}`
+    );
     console.log(response);
     return response.data;
   } catch (error: any) {
     console.log(error);
-    
   }
 }
