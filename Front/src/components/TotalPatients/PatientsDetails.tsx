08c4e6da2c9d23fbd4b140ff43efd2e1aaccb92d
manuochoa16 <nicomanu2316@gmail.com>
2024-07-17 18:54:56 -0300

    Modificacion de Modal 2

--- Front/src/components/TotalPatients/PatientsDetails.tsx
+++ Front/src/components/TotalPatients/PatientsDetails.tsx
@@ -9,11 +9,13 @@ interface PatientsDetailsProps {
   patientId: string;
   tag: string;
   folder: string;
+  title: string;
 }
 
 const PatientsDetails = ({
   patientId,
   tag,
+  title,
   folder,
   close,
 }: PatientsDetailsProps & { close: () => void }) => {
@@ -47,8 +49,6 @@ const PatientsDetails = ({
     };
     parsePatientMail();
   });
-
-  // Log history to verify URLs
 
   const openLoadClick = () => {
     setOpen(false);
@@ -116,37 +116,66 @@ const PatientsDetails = ({
     }
   };
   return (
-    <div className="relative bg-white text-red-800 flex flex-col justify-center align-middle ">
-      <div className="">
-        <button onClick={handleClick}>Ver {`${tag}`}</button>
-        <button onClick={openLoadClick}>Subir {`${tag}`}</button>
-        <button onClick={close}>X</button>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="relative bg-[#1A2228] text-white flex flex-col justify-center align-middle w-11/12 md:w-3/4 lg:w-1/2 p-4 rounded-lg shadow-lg">
+        <button
+          onClick={close}
+          className="absolute top-2 right-4 text-white" // Añadido mt-4 y mr-4
+        >
+          X
+        </button>
+        <div className="mb-4">
+          <h2 className="text-center text-xl font-semibold">Nueva orden</h2>
+        </div>
+        <div className="flex justify-around mb-4 items-center text-center space-x-2">
+          <button
+            onClick={handleClick}
+            className="bg-[#00CE90] text-white px-4 py-2 rounded"
+          >
+            Ver {`${tag}`}
+          </button>
+          <button
+            onClick={openLoadClick}
+            className="bg-[#00CE90] text-white px-4 py-2 rounded flex items-center"
+          >
+            Subir {`${tag}`}
+          </button>
+        </div>
+        {open && (
+          <div className="flex flex-col">
+            <h2 className="text-lg font-bold mb-2">{`${title}`}</h2>
+            {history.length === 0 ? (
+              <p>No hay {`${tag}`}</p>
+            ) : (
+              history.map((h, index) => (
+                <img key={index} src={h} className="mb-2 max-h-96" />
+              ))
+            )}
+          </div>
+        )}
+        {openLoad && (
+          <div className="flex flex-col">
+            <label htmlFor="profileImage" className="cursor-pointer">
+              <div className="flex flex-row">
+                <img
+                  src="https://res.cloudinary.com/ddpohfyur/image/upload/v1721251199/imagen_para_cargar_b40pek.png"
+                  alt="Subir"
+                  className="mr-2 h-6 w-6"
+                />
+                <p className="text-lg font-bold mb-2">Subir {`${tag}`}</p>
+              </div>
+              <input
+                type="file"
+                id="profileImage"
+                name="profileImage"
+                accept="image/*"
+                className="hidden"
+                onChange={(event) => handleImageUpload(event, folder)}
+              />
+            </label>
+          </div>
+        )}
       </div>
-      {open && (
-        <div className="flex flex-col">
-          <h2>{`${tag}`}</h2>
-          {history.length === 0 ? (
-            <p>No hay {`${tag}`}</p>
-          ) : (
-            history.map((h, index) => <img key={index} src={h} />)
-          )}
-        </div>
-      )}
-      {openLoad && (
-        <div className="flex flex-col">
-          <label htmlFor="profileImage" className="cursor-pointer">
-            <h2>Subir {`${tag}`}</h2>
-            <input
-              type="file"
-              id="profileImage"
-              name="profileImage"
-              accept="image/*"
-              className="hidden"
-              onChange={(event) => handleImageUpload(event, folder)}
-            />
-          </label>
-        </div>
-      )}
     </div>
   );
 };
