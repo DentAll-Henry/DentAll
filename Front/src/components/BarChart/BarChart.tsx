a6306b432a3b543873ad6024982526c389c7a2cc
rafagarfield <mgrafaelgarcia.2805@gmail.com>
2024-07-17 16:22:52 -0500

    actualizacion de informacion del superadmin

--- Front/src/components/BarChart/BarChart.tsx
+++ Front/src/components/BarChart/BarChart.tsx
@@ -1,118 +1,50 @@
-// "use client";
-// import { useEffect, useRef, RefObject } from "react";
-// import { Chart, ChartConfiguration } from "chart.js/auto";
 
-// function BarChart() {
-//     const chartRef: RefObject<HTMLCanvasElement> = useRef<HTMLCanvasElement>(null);
-
-//     useEffect(() => {
-//         if (chartRef.current) {
-//             if ((chartRef.current as any).chart) {
-//                 (chartRef.current as any).chart.destroy();
-//             }
-
-//             const context = chartRef.current?.getContext("2d");
-//             if (context) {
-//                 const config: ChartConfiguration<'bar'> = {
-//                     type: "bar",
-//                     data: {
-//                         labels: ["Pacientes", "Dentistas", "Administrativos", "Citas"],
-//                         datasets: [{
-//                             label: "cantidad",
-//                             data: [4, 4, 3, 2],
-//                             backgroundColor: [
-//                                 "rgba(255, 99, 132, 0.7)",
-//                                 "rgba(255, 159, 64, 0.7)",
-//                                 "rgba(255, 205, 86, 0.7)",
-//                                 "rgba(255, 1, 86, 0.7)",
-//                             ],
-//                             borderColor: [
-//                                 "rgb(255, 99, 132)",
-//                                 "rgb(255, 159, 64)",
-//                                 "rgb(255, 205, 86)",
-//                                 "rgb(255, 1, 86)",
-//                             ],
-//                             borderWidth: 1,
-//                         }],
-//                     },
-//                     options: {
-//                         responsive: true,
-//                         scales: {
-//                             x: {
-//                                 type: "category",
-//                                 // ticks: {
-//                                 //     color: "white", // Color de las etiquetas del eje x
-//                                 //     font: {
-//                                 //         weight: "bold", // Texto en negrita
-//                                 //     },
-//                                 // },
-//                             },
-//                             y: {
-//                                 beginAtZero: true,
-//                                 grid: {
-//                                     color: "rgba(255, 255, 255, 0.1)", // Color blanco para las líneas horizontales
-//                                 },
-//                                 // ticks: {
-//                                 //     color: "white", // Color de las etiquetas del eje y
-//                                 //     font: {
-//                                 //         weight: "bold", // Texto en negrita
-//                                 //     },
-//                                 // },
-//                             },
-//                         },
-//                         plugins: {
-//                             legend: {
-//                                 display: false, // Ocultar la leyenda
-//                                 labels: {
-//                                     color: "white", // Color del texto de la leyenda
-//                                     font: {
-//                                         weight: "bold", // Texto en negrita
-//                                     },
-//                                 },
-//                             },
-//                             title: {
-//                                 display: true,
-//                                 text: "Resumen Estadístico",
-//                                 color: "white", // Color del texto del título
-//                                 font: {
-//                                     size: 18, // Tamaño del texto del título
-//                                     weight: "bold", // Texto en negrita
-//                                 },
-//                             },
-//                         },
-//                         datasets: {
-//                             bar: {
-//                                 barThickness: 40, // Ajusta el valor para hacer las barras más delgadas o gruesas
-//                             },
-//                         },
-//                     },
-//                 };
-
-//                 const newChart = new Chart(context, config);
-//                 (chartRef.current as any).chart = newChart;
-//             }
-//         }
-//     }, []);
-
-//     return (
-//         <div className="relative w-[80%] h-auto">
-//             <canvas ref={chartRef} />
-//         </div>
-//     );
-// }
-
-// export default BarChart;
 "use client";
-import { useEffect, useRef, RefObject } from "react";
+
+import { useEffect, useRef, RefObject, useState } from "react";
 import { Chart, ChartConfiguration, registerables } from "chart.js";
+import { userSession } from "@/types";
+import { getTotalDentist, getTotalPatient, getTotalSuperAdmin, getTotalAdministrative } from "@/helpers/patients.helper";
 
 Chart.register(...registerables);
 
+interface TotalUsers {
+    total: number;
+    active: number;
+    inactive: number;
+}
+
 function BarChart() {
+    const [userData, setUserData] = useState<userSession | null>(null);
+    const [totalPatients, setTotalPatients] = useState<TotalUsers | null>(null);
+    const [totalDentists, setTotalDentists] = useState<TotalUsers | null>(null);
+    const [totalAdmins, setTotalAdmins] = useState<TotalUsers | null>(null);
+    const [totalSuperAdmins, setTotalSuperAdmins] = useState<TotalUsers | null>(null);
+
     const chartRef: RefObject<HTMLCanvasElement> = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
-        if (chartRef.current) {
+        if (typeof window !== 'undefined' && window.localStorage) {
+            const userData = localStorage.getItem('userSession');
+            setUserData(userData ? JSON.parse(userData) : null);
+        }
+
+        const fetchData = async () => {
+            const totalPatient = await getTotalPatient();
+            setTotalPatients(totalPatient);
+            const totalDentist = await getTotalDentist();
+            setTotalDentists(totalDentist);
+            const totalAdmin = await getTotalAdministrative();
+            setTotalAdmins(totalAdmin);
+            const totalSuperAdmin = await getTotalSuperAdmin();
+            setTotalSuperAdmins(totalSuperAdmin);
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (chartRef.current && totalPatients && totalDentists && totalAdmins && totalSuperAdmins) {
             if ((chartRef.current as any).chart) {
                 (chartRef.current as any).chart.destroy();
             }
@@ -125,7 +57,7 @@ function BarChart() {
                         const { ctx } = chart;
                         ctx.save();
                         ctx.globalCompositeOperation = 'destination-over';
-                        ctx.fillStyle = '#1D1D1D'; // Cambia el color de fondo aquí
+                        ctx.fillStyle = '#1D1D1D';
                         ctx.fillRect(0, 0, chart.width, chart.height);
                         ctx.restore();
                     }
@@ -134,24 +66,24 @@ function BarChart() {
                 const config: ChartConfiguration<'bar'> = {
                     type: "bar",
                     data: {
-                        labels: ["Pacientes", "Dentistas", "Administrativos", "Citas"],
+                        labels: ["Pacientes", "Dentistas", "Administrativos", "SuperAdmins"],
                         datasets: [{
                             label: "cantidad",
-                            data: [40, 4, 30, 10],
+                            data: [totalPatients.total, totalDentists.total, totalAdmins.total, totalSuperAdmins.total],
                             backgroundColor: [
                                 "rgba(255, 99, 132, 0.7)",
                                 "rgba(255, 159, 64, 0.7)",
                                 "rgba(255, 205, 86, 0.7)",
-                                "rgba(255, 1, 86, 0.7)",
+                                "rgba(54, 162, 235, 0.7)"
                             ],
                             borderColor: [
                                 "rgb(255, 99, 132)",
                                 "rgb(255, 159, 64)",
                                 "rgb(255, 205, 86)",
-                                "rgb(255, 1, 86)",
+                                "rgb(54, 162, 235)"
                             ],
                             borderWidth: 1,
-                            barThickness: 40, // Ajusta el ancho de las barras aquí
+                            barThickness: 40,
                         }],
                     },
                     options: {
@@ -163,27 +95,26 @@ function BarChart() {
                             y: {
                                 beginAtZero: true,
                                 grid: {
-                                    color: "rgba(255, 255, 255, 0.1)", // Color blanco para las líneas horizontales
+                                    color: "rgba(255, 255, 255, 0.1)",
                                 },
                             },
                         },
                         plugins: {
                             legend: {
-                                display: false, // Ocultar la leyenda
+                                display: false,
                                 labels: {
-                                    color: "white", // Color del texto de la leyenda
+                                    color: "white",
                                     font: {
-                                        weight: "bold", // Texto en negrita
+                                        weight: "bold",
                                     },
                                 },
                             },
                             title: {
                                 display: true,
                                 text: "Resumen Estadístico",
-                                // className: 'text-gray-400', // Clase de Tailwind para color del texto del título
                                 font: {
-                                    size: 18, // Tamaño del texto del título
-                                    weight: "bold", // Texto en negrita
+                                    size: 18,
+                                    weight: "bold",
                                 },
                             },
                         },
@@ -195,11 +126,11 @@ function BarChart() {
                 (chartRef.current as any).chart = newChart;
             }
         }
-    }, []);
+    }, [totalPatients, totalDentists, totalAdmins, totalSuperAdmins]);
 
     return (
-        <div className="relative w-[90%] h-auto">
-            <canvas ref={chartRef} className="rounded-xl" /> {/* Aplica border-radius y padding con Tailwind */}
+        <div className="relative w-[90%] h-auto mt-10">
+            <canvas ref={chartRef} className="rounded-xl" />
         </div>
     );
 }
